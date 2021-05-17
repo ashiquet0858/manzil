c6b0ed3b82b0b0d95bc7596345d721ddcfec2ce8
ashiquet0858 <ashiquet0858@gmail.com>
2021-05-17 11:07:41 +0530

    uodate commnet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  rootUrl:'https://manzilstrapi.herokuapp.com/'
+  rootUrl:'https://manzilbackend.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  rootUrl:'https://manzilstrapi.herokuapp.com'
+  rootUrl:'https://manzilbackend.herokuapp.com'
 
 };
 
